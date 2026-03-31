043ea097b567a8d2ab5f047126a7ca5458b41461
Sylvain Arbaudie <sylvain@arbaudie.it>
2026-03-31 20:19:57 +0200

    Update handle_post_request.c

--- code/handle_post_request.c
+++ code/handle_post_request.c
@@ -8,8 +8,16 @@ cJSON *handle_post_request(const char *url,
     (void)upload_data_size;
     cJSON *r = cJSON_CreateObject();
     if (r == NULL) return NULL;
+
     cJSON_AddStringToObject(r, "url", url ? url : "");
+
+#if POSTMETHODCORK == 0
     http_set_error(r, "POST not available in this version",
+                   HTTP_METHOD_NOT_ALLOWED);  
+#else
+    http_set_error(r, "", 
                    HTTP_METHOD_NOT_ALLOWED);
+#endif
+    
     return r;
 }
