e0b568ea9de24c1cf38a4beb6669d5e6591d9db8
Sylvain Arbaudie <sylvain@arbaudie.it>
2026-05-20 16:36:57 +0200

    Update rest2sql.c

--- code/rest2sql.c
+++ code/rest2sql.c
@@ -167,8 +167,8 @@ static int rest2sql_init(void *p)
         &http_request_handler, NULL,
         MHD_OPTION_SOCK_ADDR, &addr,
         /* MHD_OPTION_THREAD_POOL_SIZE, 5, */  /* TODO: system variable */
-        MHD_OPTION_NOTIFY_CONNECTION, connection_started,
-                                      connection_finished,
+        MHD_OPTION_NOTIFY_CONNECTION, connection_start,
+                                      connection_finish,
                                       NULL,
         MHD_OPTION_END
     );
