4f09c1e973ee664d2416dcbe677aa5740a6f01d4
Sylvain Arbaudie <sylvain@arbaudie.it>
2026-05-20 15:44:47 +0200

    Update handle_post_request.c

--- code/handle_post_request.c
+++ code/handle_post_request.c
@@ -13,10 +13,10 @@ cJSON *handle_post_request(const char *url,
 
 #if POSTMETHODCORK == 0
     http_set_error(r, "POST not available",
-                   HTTP_METHOD_NOT_ALLOWED);  
+                   HTTP_NOT_FOUND);  
 #else
     http_set_error(r, "POST disabled", 
-                   HTTP_METHOD_NOT_ALLOWED);
+                   HTTP_NOT_FOUND);
 #endif
     
     return r;
