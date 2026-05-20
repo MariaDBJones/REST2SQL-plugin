c087efea75d5055dd7195461961f6f34a18c5831
Sylvain Arbaudie <sylvain@arbaudie.it>
2026-05-20 15:57:15 +0200

    Update handle_mariadb.c

--- code/handle_mariadb.c
+++ code/handle_mariadb.c
@@ -7,3 +7,20 @@ MYSQL *handle_authentication_request(struct MHD_Connection *connection)
     (void)connection;
     return g_conn;
 }
+
+static void connection_started(void *cls,
+                                struct MHD_Connection *connection,
+                                void **socket_context)
+{
+    (void)cls; (void)connection; (void)socket_context;
+    mysql_thread_init();
+}
+
+static void connection_finished(void *cls,
+                                 struct MHD_Connection *connection,
+                                 void **socket_context,
+                                 enum MHD_RequestTerminationCode toe)
+{
+    (void)cls; (void)connection; (void)socket_context; (void)toe;
+    mysql_thread_end();
+}
